9630a1feb7a7d15c3748e6d634b6e9d46524a9e1
zunaid-28 <zunaidchauhan39@gmail.com>
2025-01-12 15:46:32 +0000

    Datattypes intro

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,31 @@
+// Day-02!
+
+"use strict"; //Treat all JS code as newer version
+
+/*console.log(2+3) console.log("monu")
+Above syntax is wrong and will throw an error, you should write a
+code which is readable and understable*/
+
+// alert(3+3) this is used in browsers not in nodejs
+
+let name = "zunaid" // String
+let Id = 12243; // Number / integer
+let isLoggedin = false; // Boolean
+let State = null
+
+// Number range => 2 to power 53;
+// Bigint is used for very big numbers;
+/* String => "" we can use single or Double quotes,
+ preferrable: Double quotes.*/
+//Boolean=> True or false?
+//NULL => Standalone value, not zero
+//Undefined => Value is not defined
+//Symbol => Further used in ReactJs, used for uniqueness
+
+
+//Objects
+
+console.log(typeof null);
+
+//ECMA standards are standard documentation of javascript;
+
