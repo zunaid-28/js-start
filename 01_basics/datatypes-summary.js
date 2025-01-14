18a263798a38108899b5b2886940d3bbe72043f7
zunaid-28 <zunaidchauhan39@gmail.com>
2025-01-14 20:51:32 +0000

    Stack and heap memory

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -1,7 +1,7 @@
 //  Primitive
 
 //  7 types : String, Number, Boolearn, null, undefined, Symbol, BigInt
-
+ 
 const score = 100
 const scoreValue = 100.3
 
@@ -14,8 +14,7 @@ const anotherId = Symbol('123')
 
 console.log(id === anotherId);
 
-// const bigNumber = 3456543576654356754n
-
+// const bigNumber = 3456543576654356754
 
 
 // Reference (Non primitive)
@@ -35,3 +34,30 @@ const myFunction = function(){
 console.log(typeof anotherId);
 
 // https://262.ecma-international.org/5.1/#sec-11.4.3
+
+
+/*=================Stack & heap memory==================== */
+
+// STACK Memory (Primitive Data types.)
+
+let MyEmail = "zunaid@gmail.com"
+let MyEmail2 = MyEmail
+
+MyEmail2 = "monu@google.com" // Email updated in 2nd variable
+ console.log(MyEmail2); // Email updated only in MyEmail2 bcz stack gives the copy to 2nd variable of 1st var..
+ console.log(MyEmail); // 
+ 
+
+ //Heap memory
+
+ let MyDetails1 = {
+      Name: "Monu",
+      contact: 4772
+ }
+
+ let MyDetails2 = MyDetails1;
+ MyDetails2.Name = "Zunaid"
+
+ console.log(MyDetails1.Name); // here 1st and 2nd variable both point to same memory in heap(Reference of var) thats why change occurs in that reference. 
+ 
+ 
